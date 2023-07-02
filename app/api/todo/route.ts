56af3ea1dff39ed9f0b079a8ee42098c3844d175
hamza <hk48709@gmail.com>
2023-07-02 13:26:57 +0500

    new data added

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,8 +1,53 @@
-import { NextResponse } from "next/server";
-export  function  GET(req:Request){
-    return NextResponse.json(
-        {
-            messgage:"DOne"
-        }
-    )
+import { NextRequest, NextResponse } from "next/server";
+import {db} from "@vercel/postgres"
+import { error } from "console";
+export async  function  GET(req:Request){
+    const client=await db.connect();
+
+    try {
+        await client.sql`CREATE TABLE if NOT EXISTS Todos (id serial,task varchar(255));`
+        const getall=await client.sql`SELECT * FROM Todos;`
+        return NextResponse.json({
+            data:getall.rows.find((item)=> item.id===2
+            
+            )
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({message:"something went wrong"});
+        
+    }
+    return NextResponse.json({
+        message:"you called this api"
+    })  
+}
+
+
+export async function POST(req:NextRequest) {
+    const reqq=await req.json();
+    const client=await db.connect();
+  try {
+    if(reqq.task){
+       
+
+       const r= await client.sql`INSERT INTO Todos (Task) VALUES (${reqq.task});`
+       console.log(r);
+        return NextResponse.json({
+            "messgae":"Data is added"
+        })
+        
+    }
+    else{
+        throw new Error("task field is requried");
+    }
+    
+  } catch (error) {
+    return NextResponse.json({
+        message:(error as {message:string}).message
+    })
+    
+  }
+
+    
 }
